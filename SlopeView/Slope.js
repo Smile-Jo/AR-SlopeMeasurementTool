914b5dcdda594a46eb4ab2a77d0f1aa2be5c3fc2
Smile-Jo <rok519674272@gmail.com>
2024-09-06 17:55:30 +0900

    a
    a

--- SlopeView/Slope.js
+++ SlopeView/Slope.js
@@ -33,23 +33,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // AR objects
     // 직각삼각형 모양 정의
-    const shape = new THREE.Shape();
-    shape.moveTo(0, 0);  // 첫 번째 꼭짓점
-    shape.lineTo(baseLength, 0);  // 두 번째 꼭짓점 (직각점)
-    shape.lineTo(baseLength, heightLength);  // 세 번째 꼭짓점
-    shape.lineTo(0, 0);  // 삼각형 닫기
+    const shape1 = new THREE.Shape();
+    shape.moveTo(-(baseLength / 2), -(1 / 2));  // 첫 번째 꼭짓점
+    shape.lineTo(baseLength / 2, -(1 / 2));  // 두 번째 꼭짓점 (직각점)
+    shape.lineTo(baseLength / 2, heightLength -(1/2));  // 세 번째 꼭짓점
+    shape.lineTo(-(baseLength / 2), -(1 / 2));  // 삼각형 닫기
+
+    const shape2 = new THREE.Shape();
+    shape.moveTo(-(baseLength / 2), 1 / 2);  // 첫 번째 꼭짓점
+    shape.lineTo(baseLength / 2, 1 / 2);  // 두 번째 꼭짓점 (직각점)
+    shape.lineTo(baseLength / 2, heightLength + 1 / 2);  // 세 번째 꼭짓점
+    shape.lineTo(-(baseLength / 2), + 1 / 2);  // 삼각형 닫기
 
     const geometry1 = new THREE.PlaneGeometry(baseLength, 1);
     const geometry2 = new THREE.PlaneGeometry(heightLength, 1);
     const geometry3 = new THREE.PlaneGeometry(hypotenuseLength, 1); // Hypotenuse plane
-    const geometry4 = new THREE.ShapeGeometry(shape);
+    const geometry4 = new THREE.ShapeGeometry(shape1);
+    const geometry5 = new THREE.ShapeGeometry(shape2);
 
     const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 });
     const plane1 = new THREE.Mesh(geometry1, material);
     const plane2 = new THREE.Mesh(geometry2, material);
     const plane3 = new THREE.Mesh(geometry3, material); // Hypotenuse plane
     const triangle1 = new THREE.Mesh(geometry4, material);
-    const triangle2 = new THREE.Mesh(geometry4, material);
+    const triangle2 = new THREE.Mesh(geometry5, material);
 
     // Set plane positions
     plane2.position.set(baseLength / 2, 0, heightLength / 2); // plane1's end to start, vertically placed
@@ -58,13 +65,14 @@ document.addEventListener('DOMContentLoaded', () => {
     plane3.position.set(0, 0, heightLength / 2); // Position plane3 correctly
     plane3.rotation.y = -Math.atan2(heightLength, baseLength); // Rotate the hypotenuse
 
+    triangle1.rotation.x = THREE.MathUtils.degToRad(90);
     triangle2.rotation.x = THREE.MathUtils.degToRad(90);
 
     const anchor = mindarThree.addAnchor(0);
-    // anchor.group.add(plane1);
-    // anchor.group.add(plane2);
-    // anchor.group.add(plane3);
-    // anchor.group.add(triangle1);
+    anchor.group.add(plane1);
+    anchor.group.add(plane2);
+    anchor.group.add(plane3);
+    anchor.group.add(triangle1);
     anchor.group.add(triangle2);
 
     // Start AR and show the camera feed as the background
